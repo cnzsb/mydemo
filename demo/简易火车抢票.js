1576bb35a4dd1298d93185543c6edd5254d42ef4
cnzsb <cnzsb@foxmail.com>
2015-12-07 18:52:58 +0800

    增加说明，在车票预订页面直接运行代码即可使用

--- demo/简易火车抢票.js
+++ demo/简易火车抢票.js
@@ -1,8 +1,11 @@
 var search = document.querySelector('#query_ticket'),
 	canOrder = document.querySelectorAll('.btn72'),
-	trains = [];
+	trains = [],
+	interval = 200;		// 默认200ms进行一次查询，可修改
+
 function ticket(train, i) {
-	// 硬座，二等座
+	// 1商务座 2特等座 3一等座 4二等座 5高级软卧 6软卧 7硬卧 8软座 9硬座 10无座
+	// 此处为硬座或二等座, 可根据需求添加
 	return train[i].parentNode.parentNode.querySelectorAll('td')[9].innerHTML
 			|| parentNode.parentNode.querySelectorAll('td')[4].innerHTML;
 }
@@ -10,7 +13,7 @@ function ticket(train, i) {
 (function timer(){
 	setTimeout(function(){
 		search.click();
-		if(canOrder){
+		if(canOrder.length != 0){
 			// 对所有有硬座或者二等座的车就行抢票
 			for (var i = 0; i < canOrder.length; i++) {
 				if(ticket(canOrder, i) != ('无' && '-')) {
@@ -27,5 +30,5 @@ function ticket(train, i) {
 		} else {
 			timer();
 		}
-	}, 200)
+	}, interval)
 })()
