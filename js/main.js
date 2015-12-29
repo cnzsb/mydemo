1d39fb3861ec3def6126daabff353da621acbd91
cnzsb <cnzsb@foxmail.com>
2015-12-29 17:16:29 +0800

    追加首页判定规则

--- js/main.js
+++ js/main.js
@@ -7,18 +7,27 @@ $(function () {
 	var $demos = $('#content').find('a'),
 		$demoslen = $demos.length,
 		$newest = $('#newest'),
-		reg = new RegExp("^demo\/(.*)\.html$", "i");
+		reg = new RegExp('^demo\\/(.*)\\.html$', 'i');
 		// console.log(reg.exec($demos.attr('href'))[1]);
 
-		$demos.each(function (i) {
+		$demos.each (function (i) {
 			var $this = $(this),
 				$num = $demoslen - i + '. ',
 				$demoname = reg.exec($this.attr('href'))[1];
-			
+			// console.log($demoname);
+			// $num = $num < 10 ? '&nbsp;&nbsp;' + $num : $num;
+
+			// 筛选文件夹类的项目
+			var r = new RegExp('(^.*)\\/(.*$)'),
+				isFile = r.exec($demoname);
+			if (isFile != null) {
+				$demoname = isFile[2] == 'index' ? isFile[1] : isFile[1] + '-' + isFile[2];
+			}
+
 			$this.html($num + $demoname + '<br />');
 
 			// 最近的Demo
-			if(i == 0){
+			if (i == 0) {
 				$newest.attr('href', reg.exec($this.attr('href'))[0])
 					.html($demoname);
 			}
