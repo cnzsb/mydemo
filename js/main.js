23f1e1f63635c7376e0e60ae40e9087ab7c981b4
cnzsb <cnzsb@foxmail.com>
2016-09-03 23:27:36 +0800

    修改链接规则

--- js/main.js
+++ js/main.js
@@ -5,34 +5,44 @@
 
 $(function () {
 	var $demos = $('#content').find('a'),
-		$demoslen = $demos.length,
+		demoslen = $demos.length,
 		$newest = $('#newest'),
 		reg = new RegExp('^demo\\/(.*)\\.html$', 'i');
 		// console.log(reg.exec($demos.attr('href'))[1]);
 
 		$demos.each (function (i) {
 			var $this = $(this),
-				$num = $demoslen - i + '. ',
-				$demoname = reg.exec($this.attr('href'))[1];
-			// console.log($demoname);
-			// $num = $num < 10 ? '&nbsp;&nbsp;' + $num : $num;
+				num = demoslen - i + '. ',
+				hasName = !!$this.html(),
+				demoname = $this.html() || reg.exec($this.attr('href'))[1];
+			// console.log(demoname);
+			// num = num < 10 ? '&nbsp;&nbsp;' + num : num;
 
 			// 给当前demo添加新窗口打开
 			$this.attr('target', '_blank');
 
-			// 筛选文件夹类的项目
-			var r = new RegExp('(^.*)\\/(.*$)'),
-				isFile = r.exec($demoname);
-			if (isFile != null) {
-				$demoname = isFile[2] == 'index' ? isFile[1] : isFile[1] + '-' + isFile[2];
+
+			if (hasName) {
+				$this.html(num + demoname + '<br />');
+				$newest.attr('href', $this.attr('href'))
+					.html(demoname);
+
+				return;
 			}
 
-			$this.html($num + $demoname + '<br />');
+			// 筛选文件夹类的项目
+			var r = new RegExp('(^.*)\\/(.*$)'),
+				isFile = r.exec(demoname);
+			if (isFile != null) {
+				demoname = isFile[2] == 'index' ? isFile[1] : isFile[1] + '-' + isFile[2];
+			}
+
+			$this.html(num + demoname + '<br />');
 
 			// 最近的Demo
 			if (i == 0) {
 				$newest.attr('href', reg.exec($this.attr('href'))[0])
-					.html($demoname);
+					.html(demoname);
 			}
 		});
 });
